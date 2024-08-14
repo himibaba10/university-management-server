e0458827a213e09df7414a7d282906ba99709028
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-14 20:33:36 +0600

    Added logic that a new semester registration can't be created if there is any UPCOMING or ONGOING semester registration is available

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -6,10 +6,27 @@ import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistration } from './semesterRegistration.model';
 import QueryBuilder from '../../builder/QueryBuilder';
+import { RegistrationStatus } from './semesterRegistration.constant';
 
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
 ) => {
+  //Check if there is any semester registration has the status: UPCOMING or ONGOING
+  const hasAnyUpcomingOrOngoingSemesterRegistration =
+    await SemesterRegistration.findOne({
+      $or: [
+        { status: RegistrationStatus.UPCOMING },
+        { status: RegistrationStatus.UPCOMING },
+      ],
+    });
+
+  if (hasAnyUpcomingOrOngoingSemesterRegistration) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `There is already an ${hasAnyUpcomingOrOngoingSemesterRegistration.status} semester registration`,
+    );
+  }
+
   const academicSemester = await AcademicSemester.findById(
     payload.academicSemester,
   );
