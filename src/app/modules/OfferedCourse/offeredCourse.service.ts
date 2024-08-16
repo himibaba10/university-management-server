2a604845ec643a1de17ad1c034048317c83f0965
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-16 17:27:36 +0600

    Added validations before creating an offered course in service so every objectId is available

--- src/app/modules/OfferedCourse/offeredCourse.service.ts
+++ src/app/modules/OfferedCourse/offeredCourse.service.ts
@@ -1,7 +1,61 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
 import { TOfferedCourse } from './offeredCourse.interface';
 import OfferedCourse from './offeredCourse.model';
+import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
+import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  const {
+    academicDepartment,
+    academicFaculty,
+    course,
+    faculty,
+    semesterRegistration,
+  } = payload;
+
+  const doesSemesterRegistrationExist =
+    await SemesterRegistration.findById(semesterRegistration);
+
+  if (!doesSemesterRegistrationExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester registration is not found.',
+    );
+  }
+
+  const doesAcademicFacultyExist =
+    await AcademicFaculty.findById(academicFaculty);
+
+  if (!doesAcademicFacultyExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Academic faculty is not found.');
+  }
+
+  const doesAcademicDepartmentExist =
+    await AcademicDepartment.findById(academicDepartment);
+
+  if (!doesAcademicDepartmentExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Academic department is not found.',
+    );
+  }
+
+  const doesFacultyExist = await SemesterRegistration.findById(faculty);
+
+  if (!doesFacultyExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Faculty is not found.');
+  }
+
+  const doesCourseExist = await SemesterRegistration.findById(course);
+
+  if (!doesCourseExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course is not found.');
+  }
+
+  payload.academicSemester = doesSemesterRegistrationExist.academicSemester;
+
   const result = await OfferedCourse.create(payload);
   return result;
 };
