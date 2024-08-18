33320c54295b0ad7200f2e66bde61b0a883fc86a
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-08-18 20:36:38 +0600

    Added logic to have endTime must be greater than startTime

--- src/app/modules/OfferedCourse/offeredCourse.validation.ts
+++ src/app/modules/OfferedCourse/offeredCourse.validation.ts
@@ -2,34 +2,47 @@ import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
 const createOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    maxCapacity: z.number().int().positive(),
-    section: z.number().int().positive(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string().refine(
-      (val) => {
-        const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
-        return regex.test(val);
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      maxCapacity: z.number().int().positive(),
+      section: z.number().int().positive(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])),
+      startTime: z.string().refine(
+        (val) => {
+          const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+          return regex.test(val);
+        },
+        {
+          message: 'Invalid time format. Please use HH:MM (24-hour) format.',
+        },
+      ),
+      endTime: z.string().refine(
+        (val) => {
+          const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+          return regex.test(val);
+        },
+        {
+          message: 'Invalid time format. Please use HH:MM (24-hour) format.',
+        },
+      ),
+    })
+    .refine(
+      (body) => {
+        //map(Number) returns strings as numbers! Awesome
+        const [startHour, startMin] = body.startTime.split(':').map(Number);
+        const [endHour, endMin] = body.endTime.split(':').map(Number);
+
+        return (
+          startHour < endHour || (startHour === endHour && startMin <= endMin)
+        );
       },
-      {
-        message: 'Invalid time format. Please use HH:MM (24-hour) format.',
-      },
+      { message: 'End time must be greater than start time' },
     ),
-    endTime: z.string().refine(
-      (val) => {
-        const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
-        return regex.test(val);
-      },
-      {
-        message: 'Invalid time format. Please use HH:MM (24-hour) format.',
-      },
-    ),
-  }),
 });
 
 const updateOfferedCourseValidationSchema = z.object({
