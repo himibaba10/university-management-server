697ef540e16a51f03717947bbc9a14b0ff748f94
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-10-15 02:02:11 +0600

    checked if user is not found, is deleted or blocked

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,32 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 
-const loginUserFromDB = (payload: TLoginUser) => {
-  const result = payload;
-  console.log(result);
-  return result;
+const loginUserFromDB = async (payload: TLoginUser) => {
+  const { id } = payload;
+
+  const doesUserExist = await User.findOne({ id });
+
+  if (!doesUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+
+  const isDeleted = doesUserExist?.isDeleted;
+
+  if (isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is deleted');
+  }
+
+  const userStatus = doesUserExist?.status;
+
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is blocked');
+  }
+
+  console.log(doesUserExist);
+
+  return {};
 };
 
 export const authServices = {
